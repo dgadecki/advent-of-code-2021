bd38a18044d279d561c26fca3de70d9b909d80cc
Damian Gadecki <damian.gadecki@picard.de>
2021-12-01 06:24:05 +0100

    Add the solution for day 1

--- src/main/java/day1/Solution.java
+++ src/main/java/day1/Solution.java
@@ -10,8 +10,39 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) {
-        InputReader inputReader = new InputReader();
-        List<String> input = inputReader.readInput("input_day1");
-        System.out.println(input.stream().toList());
+        System.out.println("First result:" + firstPart());
+        System.out.println("Second result:" + secondPart());
+    }
+
+    public static int firstPart() {
+        List<Long> input = new InputReader().readInput("input_day1")
+                .stream()
+                .map(Long::valueOf)
+                .toList();
+
+        int result = 0;
+        for (int i = 0; i < input.size() - 1; i++) {
+            if (input.get(i) < input.get(i + 1)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static int secondPart() {
+        List<Long> input = new InputReader().readInput("input_day1")
+                .stream()
+                .map(Long::valueOf)
+                .toList();
+
+        int result = 0;
+        for (int i = 2; i < input.size() - 1; i++) {
+            long firstWindowValue = input.get(i - 2) + input.get(i - 1) + input.get(i);
+            long secondWindowValue = input.get(i - 1) + input.get(i) + input.get(i + 1);
+            if (firstWindowValue < secondWindowValue) {
+                result++;
+            }
+        }
+        return result;
     }
 }
