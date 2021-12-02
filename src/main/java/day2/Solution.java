ac5e12e268987bc89b2a424508e983f8c4ac8d2d
Damian Gadecki <damian.gadecki@picard.de>
2021-12-02 06:22:25 +0100

    Add the solution for day 2

--- src/main/java/day2/Solution.java
+++ src/main/java/day2/Solution.java
@@ -10,17 +10,51 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("First result:" + firstPart());
-        System.out.println("Second result:" + secondPart());
+        System.out.println("First result: " + firstPart());
+        System.out.println("Second result: " + secondPart());
     }
 
     public static int firstPart() {
         List<String> inputAsString = new InputReader().readInput("input_day2");
-        return 0;
+        int horizontal = 0;
+        int depth = 0;
+
+        for (String input : inputAsString) {
+            String course = input.split(" ")[0];
+            int courseValue = Integer.parseInt(input.split(" ")[1]);
+            if ("forward".equals(course)) {
+                horizontal += courseValue;
+            }
+            if ("down".equals(course)) {
+                depth += courseValue;
+            }
+            if ("up".equals(course)) {
+                depth -= courseValue;
+            }
+        }
+        return horizontal * depth;
     }
 
     public static int secondPart() {
         List<String> inputAsString = new InputReader().readInput("input_day2");
-        return 0;
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+
+        for (String input : inputAsString) {
+            String course = input.split(" ")[0];
+            int courseValue = Integer.parseInt(input.split(" ")[1]);
+            if ("forward".equals(course)) {
+                horizontal += courseValue;
+                depth += (aim * courseValue);
+            }
+            if ("down".equals(course)) {
+                aim += courseValue;
+            }
+            if ("up".equals(course)) {
+                aim -= courseValue;
+            }
+        }
+        return horizontal * depth;
     }
 }
