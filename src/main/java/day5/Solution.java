35c6a2cad5c262f7c5549bc4ed35fa3a4f6dd67c
Damian Gadecki <damian.gadecki@picard.de>
2021-12-04 06:49:06 +0100

    Add the solution for day 5

---
+++ src/main/java/day5/Solution.java
@@ -0,0 +1,27 @@
+package day5;
+
+import common.InputReader;
+
+import java.util.List;
+
+/**
+ * Advent of code - day 5
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("First result: " + firstPart());
+        System.out.println("Second result: " + secondPart());
+    }
+
+    public static int firstPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day5");
+
+        return 0;
+    }
+
+    public static int secondPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day5");
+        return 0;
+    }
+}
