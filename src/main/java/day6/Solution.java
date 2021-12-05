92c42385f196c5d3b61fa12032904c8afde5023e
Damian Gadecki <damian.gadecki@picard.de>
2021-12-05 07:47:52 +0100

    Add template for day 6

---
+++ src/main/java/day6/Solution.java
@@ -0,0 +1,29 @@
+package day6;
+
+import common.InputReader;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Advent of code - day 6
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("First result: " + firstPart());
+        System.out.println("Second result: " + secondPart());
+    }
+
+    public static int firstPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day6");
+        return 0;
+    }
+
+    public static int secondPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day6");
+        return 0;
+    }
+}
