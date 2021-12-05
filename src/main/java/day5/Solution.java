c40db32d8172ddd13fab6a096f8955502ce90205
Damian Gadecki <damian.gadecki@picard.de>
2021-12-05 07:45:35 +0100

    Add the solution for day 5

--- src/main/java/day5/Solution.java
+++ src/main/java/day5/Solution.java
@@ -15,13 +15,115 @@ public class Solution {
     }
 
     public static int firstPart() {
-        List<String> inputAsString = new InputReader().readInput("input_day5");
+        List<Coordinates> inputAsString = new InputReader().readInput("input_day5").stream()
+                .map(x -> x.replaceAll(" +", ""))
+                .map(x -> x.replaceAll("->", ","))
+                .map(x -> x.split(","))
+                .map(x -> new Coordinates(Integer.parseInt(x[0]), Integer.parseInt(x[1]), Integer.parseInt(x[2]), Integer.parseInt(x[3])))
+                .filter(c -> c.x1 == c.x2 || c.y1 == c.y2)
+                .toList();
 
-        return 0;
+        int[][] board = new int[1000][1000];
+
+        for (Coordinates coordinate : inputAsString) {
+            if (coordinate.x1 == coordinate.x2) {
+                for (int i = Math.min(coordinate.y1, coordinate.y2); i <= Math.max(coordinate.y1, coordinate.y2); i++) {
+                    board[coordinate.x1][i]++;
+                }
+            }
+            if (coordinate.y1 == coordinate.y2) {
+                for (int i = Math.min(coordinate.x1, coordinate.x2); i <= Math.max(coordinate.x1, coordinate.x2); i++) {
+                    board[i][coordinate.y1]++;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (board[i][j] >= 2) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
     }
 
     public static int secondPart() {
-        List<String> inputAsString = new InputReader().readInput("input_day5");
-        return 0;
+        List<Coordinates> verticalAndHorizontal = new InputReader().readInput("input_day5").stream()
+                .map(x -> x.replaceAll(" +", ""))
+                .map(x -> x.replaceAll("->", ","))
+                .map(x -> x.split(","))
+                .map(x -> new Coordinates(Integer.parseInt(x[0]), Integer.parseInt(x[1]), Integer.parseInt(x[2]), Integer.parseInt(x[3])))
+                .filter(c -> c.x1 == c.x2 || c.y1 == c.y2)
+                .toList();
+
+        List<Coordinates> diagonal = new InputReader().readInput("input_day5").stream()
+                .map(x -> x.replaceAll(" +", ""))
+                .map(x -> x.replaceAll("->", ","))
+                .map(x -> x.split(","))
+                .map(x -> new Coordinates(Integer.parseInt(x[0]), Integer.parseInt(x[1]), Integer.parseInt(x[2]), Integer.parseInt(x[3])))
+                .filter(c -> !(c.x1 == c.x2 || c.y1 == c.y2))
+                .toList();
+
+        int[][] board = new int[1000][1000];
+
+        for (Coordinates coordinate : verticalAndHorizontal) {
+            if (coordinate.x1 == coordinate.x2) {
+                for (int i = Math.min(coordinate.y1, coordinate.y2); i <= Math.max(coordinate.y1, coordinate.y2); i++) {
+                    board[coordinate.x1][i]++;
+                }
+            }
+            if (coordinate.y1 == coordinate.y2) {
+                for (int i = Math.min(coordinate.x1, coordinate.x2); i <= Math.max(coordinate.x1, coordinate.x2); i++) {
+                    board[i][coordinate.y1]++;
+                }
+            }
+        }
+
+        for (Coordinates c : diagonal) {
+            if (c.x1 < c.x2) {
+                for (int i = 0; i <= c.x2 - c.x1; i++) {
+                    if (c.y1 > c.y2) {
+                        board[c.x1 + i][c.y1 - i]++;
+                    } else {
+                        board[c.x1 + i][c.y1 + i]++;
+                    }
+                }
+            } else if (c.x1 > c.x2) {
+                for (int i = 0; i <= c.x1 - c.x2; i++) {
+                    if (c.y1 > c.y2) {
+                        board[c.x1 - i][c.y1 - i]++;
+                    } else {
+                        board[c.x1 - i][c.y1 + i]++;
+                    }
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (board[i][j] >= 2) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static class Coordinates {
+        public int x1;
+        public int y1;
+        public int x2;
+        public int y2;
+
+        public Coordinates(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
     }
 }
