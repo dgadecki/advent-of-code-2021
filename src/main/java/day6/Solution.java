ab404e9b5873ef00ef9475af0408d638e250e956
Damian Gadecki <damian.gadecki@picard.de>
2021-12-06 06:57:53 +0100

    Add the solution for day 6

--- src/main/java/day6/Solution.java
+++ src/main/java/day6/Solution.java
@@ -1,7 +1,6 @@
 package day6;
 
 import common.InputReader;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,11 +18,43 @@ public class Solution {
 
     public static int firstPart() {
         List<String> inputAsString = new InputReader().readInput("input_day6");
-        return 0;
+        List<Integer> values = new ArrayList<>(
+                Arrays.stream(inputAsString.get(0).split(",")).map(Integer::parseInt).toList()
+        );
+
+        for (int i = 0; i < 80; i++) {
+            List<Integer> modifiedValues = new ArrayList<>();
+            for (Integer value : values) {
+                if (value == 0) {
+                    modifiedValues.add(6);
+                    modifiedValues.add(8);
+                } else {
+                    modifiedValues.add(value - 1);
+                }
+            }
+            values.clear();
+            values.addAll(modifiedValues);
+            modifiedValues.clear();
+        }
+        return values.size();
     }
 
-    public static int secondPart() {
+    public static long secondPart() {
         List<String> inputAsString = new InputReader().readInput("input_day6");
-        return 0;
+        List<Integer> input = Arrays.stream(inputAsString.get(0).split(",")).map(Integer::parseInt).toList();
+
+        long[] values = new long[9];
+        for (Integer i : input) {
+            values[i]++;
+        }
+        for (int i = 0; i < 256; i++) {
+           long newValues = values[0];
+           for (int age = 0; age < 8; age++) {
+               values[age] = values[age + 1];
+           }
+            values[8] = newValues;
+            values[6] += newValues;
+        }
+        return Arrays.stream(values).sum();
     }
 }
