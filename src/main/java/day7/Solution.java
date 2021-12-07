88a193459f7b91afabf7ccb310652e05023ccf70
Damian Gadecki <damian.gadecki@picard.de>
2021-12-07 06:44:09 +0100

    Add the solution for day 7

--- src/main/java/day7/Solution.java
+++ src/main/java/day7/Solution.java
@@ -2,6 +2,8 @@ package day7;
 
 import common.InputReader;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,12 +17,40 @@ public class Solution {
     }
 
     public static int firstPart() {
-        List<String> inputAsString = new InputReader().readInput("input_day7");
-        return 0;
+        List<Integer> positions = Arrays.stream(new InputReader().readInput("input_day7").get(0).split(",")).map(Integer::parseInt).toList();
+        int maxPosition = Collections.max(positions);
+        int minPosition = Collections.min(positions);
+        int best = Integer.MAX_VALUE;
+
+        for (int i = minPosition; i < maxPosition; i++) {
+            int fuel = 0;
+            for(Integer position : positions) {
+                fuel += Math.abs(position - i);
+            }
+            if (best > fuel) {
+                best = fuel;
+            }
+        }
+        return best;
     }
 
     public static int secondPart() {
-        List<String> inputAsString = new InputReader().readInput("input_day7");
-        return 0;
+        List<Integer> positions = Arrays.stream(new InputReader().readInput("input_day7").get(0).split(",")).map(Integer::parseInt).toList();
+        int maxPosition = Collections.max(positions);
+        int minPosition = Collections.min(positions);
+        int best = Integer.MAX_VALUE;
+
+        for (int i = minPosition; i < maxPosition; i++) {
+            int fuel = 0;
+            for(Integer position : positions) {
+                int abs = Math.abs(position - i);
+                // Suma ciągu arytmetycznego: 1, 2, 3, ..., abs = ((1 + a_n) * n) / 2)
+                fuel += ((1 + abs) * abs) / 2;
+            }
+            if (best > fuel) {
+                best = fuel;
+            }
+        }
+        return best;
     }
 }
