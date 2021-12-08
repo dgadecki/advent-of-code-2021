f59bec6baecc69308edcb5839940fb939ea659b7
Damian Gadecki <damian.gadecki@picard.de>
2021-12-08 08:32:16 +0100

    Add template for day 9

---
+++ src/main/java/day9/Solution.java
@@ -0,0 +1,26 @@
+package day9;
+
+import common.InputReader;
+
+import java.util.List;
+
+/**
+ * Advent of code - day 9
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("First result: " + firstPart());
+        System.out.println("Second result: " + secondPart());
+    }
+
+    public static int firstPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day9");
+        return 0;
+    }
+
+    public static int secondPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day9");
+        return 0;
+    }
+}
