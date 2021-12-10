c393184d88eebb84a614d8b093b4771e787532b1
Damian Gadecki <damian.gadecki@picard.de>
2021-12-10 07:17:51 +0100

    Add template for day 11

---
+++ src/main/java/day11/Solution.java
@@ -0,0 +1,26 @@
+package day11;
+
+import common.InputReader;
+
+import java.util.List;
+
+/**
+ * Advent of code - day 11
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("First result: " + firstPart());
+        System.out.println("Second result: " + secondPart());
+    }
+
+    public static long firstPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day11");
+        return 0L;
+    }
+
+    public static long secondPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day11");
+        return 0L;
+    }
+}
