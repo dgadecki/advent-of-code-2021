ab736c4339427c6d1478a0ca7621ce6adc5809bd
Damian Gadecki <damian.gadecki@picard.de>
2021-12-11 07:14:15 +0100

    Add solution for day 11 and template for day 12

--- src/main/java/day11/Solution.java
+++ src/main/java/day11/Solution.java
@@ -16,11 +16,99 @@ public class Solution {
 
     public static long firstPart() {
         List<String> inputAsString = new InputReader().readInput("input_day11");
-        return 0L;
+        Octopus[][] octopus = new Octopus[10][10];
+        for (int i = 0; i < 10; i++) {
+            Octopus[] singleLine = inputAsString.get(i).chars().map(x -> x - '0').boxed().map(Octopus::new).toArray(Octopus[]::new);
+            octopus[i] = singleLine;
+        }
+
+        int numberOfSteps = 100;
+        int result = 0;
+        for(int n = 0; n < numberOfSteps; n++) {
+            result += calculateStep(octopus);
+        }
+        return result;
     }
 
     public static long secondPart() {
         List<String> inputAsString = new InputReader().readInput("input_day11");
-        return 0L;
+        Octopus[][] octopus = new Octopus[10][10];
+        for (int i = 0; i < 10; i++) {
+            Octopus[] singleLine = inputAsString.get(i).chars().map(x -> x - '0').boxed().map(Octopus::new).toArray(Octopus[]::new);
+            octopus[i] = singleLine;
+        }
+
+        int result = 1;
+        while (!isCommonFlash(octopus)) {
+            result++;
+        }
+        return result;
+    }
+
+    static boolean isCommonFlash(Octopus[][] octopus) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                calculateSingleOctopus(i, j, octopus);
+            }
+        }
+        boolean isCommonFlash = true;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (octopus[i][j].flash) {
+                    octopus[i][j].flash = false;
+                    octopus[i][j].energy = 0;
+                } else {
+                    isCommonFlash = false;
+                }
+            }
+        }
+        return isCommonFlash;
+    }
+
+    static int calculateStep(Octopus[][] octopus) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+               calculateSingleOctopus(i, j, octopus);
+            }
+        }
+        int stepResult = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (octopus[i][j].flash) {
+                    octopus[i][j].flash = false;
+                    octopus[i][j].energy = 0;
+                    stepResult += 1;
+                }
+            }
+        }
+        return stepResult;
+    }
+
+    static void calculateSingleOctopus(int i, int j, Octopus[][] octopus) {
+        if (i < 0 || j < 0 || i > 9 || j > 9) {
+            return;
+        }
+        Octopus oct = octopus[i][j];
+        oct.energy += 1;
+        if (oct.energy > 9 && !oct.flash) {
+            oct.flash = true;
+            calculateSingleOctopus(i + 1, j - 1, octopus);
+            calculateSingleOctopus(i + 1, j, octopus);
+            calculateSingleOctopus(i + 1, j + 1, octopus);
+            calculateSingleOctopus(i, j - 1, octopus);
+            calculateSingleOctopus(i, j + 1, octopus);
+            calculateSingleOctopus(i - 1, j - 1, octopus);
+            calculateSingleOctopus(i - 1, j, octopus);
+            calculateSingleOctopus(i - 1, j + 1, octopus);
+        }
+    }
+
+    static class Octopus {
+        boolean flash;
+        int energy;
+
+        public Octopus(int energy) {
+            this.energy = energy;
+        }
     }
 }
---
+++ src/main/java/day12/Solution.java
@@ -0,0 +1,26 @@
+package day12;
+
+import common.InputReader;
+
+import java.util.List;
+
+/**
+ * Advent of code - day 12
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("First result: " + firstPart());
+        System.out.println("Second result: " + secondPart());
+    }
+
+    public static long firstPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day12");
+        return 0L;
+    }
+
+    public static long secondPart() {
+        List<String> inputAsString = new InputReader().readInput("input_day12");
+        return 0L;
+    }
+}
